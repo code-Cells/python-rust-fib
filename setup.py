4d6ccba7fcc1317577962076ed2ceafaa8090ab3
code-Cells <eduardo.g.amaral1997@gmail.com>
2023-10-05 14:49:51 -0300

    Added fib Python module.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="python-rust-fib",
+    version="0.0.1",
+    author="Eduardo G. A. de Oliveira",
+    author_email="eduardo.g.amaral1997@gmail.com",
+    description="Calculates a Fibonacci number",
+    install_requires=[],
+    packages=find_packages(exclude=("tests",)),
+    python_requires='>=3',
+    tests_require=['pytest'],
+)
+
