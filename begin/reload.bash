c8ef9f4227f96934daafd7f03f102d21aea7305c
code-Cells <eduardo.g.amaral1997@gmail.com>
2023-10-05 21:05:52 -0300

    main

--- begin/reload.bash
+++ begin/reload.bash
@@ -2,6 +2,8 @@ name=$(pwd | rev | cut -d '/' -f 2 | rev)
 
 git add -A;
 git commit -m $1;
-(echo "code-Cells"; cat .gitkey) | git push origin main
+echo "code-Cells"
+echo $gitkey
+git push origin main
 pip3 install git+https://github.com/code-Cells/$name@$2
 
